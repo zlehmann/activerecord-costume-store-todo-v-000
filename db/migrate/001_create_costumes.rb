b631bb15cbd3e16cae3b50ca13f2a598bdadef1b
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 00:48:44 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,5 +5,14 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name, :image_url, :size
+      t.decimal :price
+      t.timestamps
+    end
+  end
+end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name, :location
+      t.integer :costume_inventory, :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time, :closing_time
+    end
+  end
+end
